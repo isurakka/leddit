012182da4a2e13b1d286366e2faf7f640260c80a
Iiro Surakka <iiro.surakka@gmail.com>
2014-11-21 18:37:29 +0200

    Change activity fragment name

--- app/src/main/java/com/leddit/leddit/MainActivity.java
+++ app/src/main/java/com/leddit/leddit/MainActivity.java
@@ -5,18 +5,13 @@ import android.app.Activity;
 import android.app.ActionBar;
 import android.app.Fragment;
 import android.app.FragmentManager;
-import android.content.Context;
-import android.os.Build;
 import android.os.Bundle;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.support.v4.widget.DrawerLayout;
-import android.widget.ArrayAdapter;
-import android.widget.TextView;
 
 
 public class MainActivity extends Activity
@@ -52,7 +47,7 @@ public class MainActivity extends Activity
         // update the main content by replacing fragments
         FragmentManager fragmentManager = getFragmentManager();
         fragmentManager.beginTransaction()
-                .replace(R.id.container, PlaceholderFragment.newInstance(position + 1))
+                .replace(R.id.container, ThreadListFragment.newInstance(position + 1))
                 .commit();
     }
 
@@ -109,7 +104,7 @@ public class MainActivity extends Activity
     /**
      * A placeholder fragment containing a simple view.
      */
-    public static class PlaceholderFragment extends Fragment {
+    public static class ThreadListFragment extends Fragment {
         /**
          * The fragment argument representing the section number for this
          * fragment.
@@ -120,15 +115,15 @@ public class MainActivity extends Activity
          * Returns a new instance of this fragment for the given section
          * number.
          */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
+        public static ThreadListFragment newInstance(int sectionNumber) {
+            ThreadListFragment fragment = new ThreadListFragment();
             Bundle args = new Bundle();
             args.putInt(ARG_SECTION_NUMBER, sectionNumber);
             fragment.setArguments(args);
             return fragment;
         }
 
-        public PlaceholderFragment() {
+        public ThreadListFragment() {
         }
 
         @Override
